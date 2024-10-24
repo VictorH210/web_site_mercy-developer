de082f92631a234a1030a874e95636d170f64fb0
Victor Herrera <victor.herrera.guzman@cftmail.cl>
2024-10-24 11:49:45 -0300

    Evaluacion por terminar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { ContactanosComponent } from './componentes/contactanos/contactanos.component';
+import { UbicanosComponent } from './componentes/ubicanos/ubicanos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: InicioComponent
+    
+  },
+  {
+    path:"inicio",
+    component: InicioComponent
+  },
+  {
+    path: "contactanos",
+    component: ContactanosComponent
+  },
+  {
+    path: "ubicanos",
+    component: UbicanosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { ContactanosComponent } from './componentes/contactanos/contactanos.component';
+import { UbicanosComponent } from './componentes/ubicanos/ubicanos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    ContactanosComponent,
+    UbicanosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
